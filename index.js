f8c6f4944ef839a6bf30b85a4ee28e6c175a47a2
nandanchakraborty <nandancsebubt@gmail.com>
2026-02-12 11:32:06 +0600

    learning module and export import

---
+++ index.js
@@ -0,0 +1,10 @@
+const people = ['sakib', 'tamim', 'Dhoni', 'kholi'];
+const a = 6;
+function test() {
+    console.log('test');
+}
+module.exports = {
+    people,
+    a,
+    test,
+};
---
+++ people.js
@@ -0,0 +1,5 @@
+const people = require('./index');
+
+console.log(people.people);
+console.log(people.a);
+people.test();
