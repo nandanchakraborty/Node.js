400bbe825e0b0905225099e4fdbe8771500a513f
nandanchakraborty <nandancsebubt@gmail.com>
2026-03-03 11:44:59 +0600

    this handles the request and response from the server

---
+++ RawNodeApiProject/helpers/handleReqRes.js
@@ -0,0 +1,55 @@
+// handle req res
+
+// dependencies
+const { StringDecoder } = require('string_decoder');
+const url = require('url');
+const routes = require('../routes');
+const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
+
+// scafolding
+const handler = {};
+
+handler.HandleReqRes = (req, res) => {
+    // req handle
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+
+        const requestProparties = {
+            parsedUrl,
+            path,
+            trimedPath,
+            method,
+            queryStringObject,
+            headersObject,
+            body: realData,
+        };
+
+        const chosenHandler = routes[trimedPath] ? routes[trimedPath] : notFoundHandler;
+        chosenHandler(requestProparties, (statusCode, payload) => {
+            const finalStatusCode = typeof statusCode === 'number' ? statusCode : 500;
+
+            const finalPayload = typeof payload === 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(finalPayload);
+
+            res.writeHead(finalStatusCode);
+            res.end(payloadString);
+        });
+    });
+};
+
+module.exports = handler;
