9a65c6948bcdf31304df922bc285caa9a94c1494
nandanchakraborty <nandancsebubt@gmail.com>
2026-03-09 23:35:41 +0600

    utilites function

---
+++ RawNodeApiProject/helpers/utilities.js
@@ -0,0 +1,28 @@
+// utilities function
+
+// module scaffolding
+
+const utilities = {};
+const crypto = require('crypto');
+const environment = require('./environment');
+
+utilities.parseJSON = (jsonString) => {
+    let output;
+
+    try {
+        output = JSON.parse(jsonString);
+    } catch {
+        output = {};
+    }
+    return output;
+};
+// hash string
+utilities.hash = (str) => {
+    if (typeof str === 'string' && str.length > 0) {
+        const hash = crypto.createHmac('sha256', environment.secretKey).update(str).digest('hex');
+        return hash;
+    }
+    return false;
+};
+
+module.exports = utilities;
