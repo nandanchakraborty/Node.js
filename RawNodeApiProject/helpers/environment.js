94e69a5f9648eb2bf1b5c9276af0a808423db255
nandanchakraborty <nandancsebubt@gmail.com>
2026-03-11 08:45:18 +0600

    bulit different environment for staging and production

---
+++ RawNodeApiProject/helpers/environment.js
@@ -0,0 +1,25 @@
+// module scaffolding
+const environments = {};
+
+environments.staging = {
+    port: 3000,
+    envName: 'staging',
+    secretKey: 'dlfjwioefjoeijfs',
+};
+
+environments.production = {
+    port: 5000,
+    envName: 'production',
+    secretKey: 'fjwoirfjwofjdfjwo',
+};
+
+// determine which environment was passed
+const currentEnvironment =
+    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+
+const environmentToExport =
+    typeof environments[currentEnvironment] === 'object'
+        ? environments[currentEnvironment]
+        : environments.staging;
+
+module.exports = environmentToExport;
