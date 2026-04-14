e720635f552c6cf70999afc74f7bca3e75b4b884
nandanchakraborty <nandancsebubt@gmail.com>
2026-04-14 21:38:20 +0600

    index file of TODO application

--- CRUD_Application_using_mongoose_and_express/index.js
+++ CRUD_Application_using_mongoose_and_express/index.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const todoHandler = require('./routeHandler/todoHandler')
+const todoHandler = require('./routeHandler/todoHandler');
 
 const app = express();
 
-
 app.use(express.json());
 
-// database connection with mongoose
+app.use('/todo', todoHandler);
 
+// database connection
 mongoose
-    .connect('mongodb://localhost/todos') // mongodb connection string
+    .connect('mongodb://localhost:27017/todos')
     .then(() => console.log('connection successful'))
     .catch((err) => console.log(err));
 
 function errorHandler(err, req, res, next) {
-    if (res.headersent) {
+    if (res.headersSent) {
         return next(err);
     }
-app.use('/todo',todoHandler)
-
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err.message });
 }
+
+// optional but good practice
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('app listening at port 3000');
 });
+
+// starting mongodbs : docker exec -it mongodb mongosh
